2a3f390cbba694b11b2001300e9f5f5d48c2af65
Code Nation <codenation@Codes-MacBook-Pro.local>
2019-11-29 16:50:16 +0000

    Updates

---
+++ precache-manifest.a53096ef2062a18acad8b9ff790617de.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3fa10eb485a3b675ef4e4c6710d626",
+    "url": "/TomAllen95/reactRedditProject.git/index.html"
+  },
+  {
+    "revision": "cfedc203af4f720af806",
+    "url": "/TomAllen95/reactRedditProject.git/static/css/main.85729d71.chunk.css"
+  },
+  {
+    "revision": "92c69ae5e4e1c0e97445",
+    "url": "/TomAllen95/reactRedditProject.git/static/js/2.88046f29.chunk.js"
+  },
+  {
+    "revision": "cfedc203af4f720af806",
+    "url": "/TomAllen95/reactRedditProject.git/static/js/main.dad88469.chunk.js"
+  },
+  {
+    "revision": "109e51cd0a2e7f6fc65c",
+    "url": "/TomAllen95/reactRedditProject.git/static/js/runtime-main.37139d75.js"
+  },
+  {
+    "revision": "82b16a761a1270c3b4cb579f49d95cea",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/Screenshot.82b16a76.png"
+  },
+  {
+    "revision": "d2b20a2f41d91992283dbc662690e0fe",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/Screenshot3.d2b20a2f.png"
+  },
+  {
+    "revision": "2517b39d51d1ed85587c0ba13314d6c1",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/Screenshot4.2517b39d.png"
+  },
+  {
+    "revision": "853546ca4c9050eee517c7b803dcc9ae",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/ah.853546ca.gif"
+  },
+  {
+    "revision": "49e7623e3befda6d3a4ba4df0117b89a",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/boris.49e7623e.jpg"
+  },
+  {
+    "revision": "7ea6f40a3f9e5461ca9569cde5e84790",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/cat.7ea6f40a.gif"
+  },
+  {
+    "revision": "916fdda421bef0bf2731a18d1929dd07",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/comment.916fdda4.png"
+  },
+  {
+    "revision": "89d4b9028ea93f00f7fddc2bbf4488bc",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/ducks.89d4b902.jpg"
+  },
+  {
+    "revision": "0941706af4b2c042a37d10d3fbce31f6",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/earth.0941706a.gif"
+  },
+  {
+    "revision": "dbae20aac1da7a1c2e4f8299087d9e15",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/finger.dbae20aa.gif"
+  },
+  {
+    "revision": "2efe3caca57b675d506c980f35eba798",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/image.2efe3cac.png"
+  },
+  {
+    "revision": "0382833f790ad4e8ad22aa53f617e5fd",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/jesus.0382833f.jpg"
+  },
+  {
+    "revision": "64ac67fcaf8683d1cff6626f64419d03",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/landlord.64ac67fc.jpg"
+  },
+  {
+    "revision": "22d5d6bc3f62b6a950132a84db1d59fa",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/library.22d5d6bc.jpg"
+  },
+  {
+    "revision": "72f751441bd009fbd6ec36ee0dc2d6d9",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/link.72f75144.png"
+  },
+  {
+    "revision": "8a83628b4cebd9274d05de510b6d6d88",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/news1.8a83628b.jpg"
+  },
+  {
+    "revision": "7759e30e080cb26d41c0e0e356716ecd",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/news2.7759e30e.jpg"
+  },
+  {
+    "revision": "99574e7bc89d5a91023cb7c70e105e32",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/news3.99574e7b.jpg"
+  },
+  {
+    "revision": "98be27eae92befd74caea2756aa29c22",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/news4.98be27ea.jpg"
+  },
+  {
+    "revision": "f8d7453784f044cd3b0d892a7d92bc83",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/save.f8d74537.png"
+  },
+  {
+    "revision": "080bee957911f903e1a6af11d887c0a5",
+    "url": "/TomAllen95/reactRedditProject.git/static/media/share.080bee95.png"
+  }
+]);
